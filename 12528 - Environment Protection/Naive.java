22cfd1405f00984e635a17e1dcccde75cdbbde0f
Diego Serrano <dfserrano@gmail.com>
2013-10-21 07:04:20 -0500

    Corrección a Env. Protection

--- 12528 - Environment Protection/Naive.java
+++ 12528 - Environment Protection/Naive.java
@@ -1,33 +1,63 @@
 /**
- * 12527 - DIFFERENT DIGITS
- * http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=3972
- *
- * The inhabitants of Nlogonia are very superstitious. One of their beliefs is that 
- * street house numbers that have a repeated digit bring bad luck for the residents. 
- * Therefore, they would never live in a house which has a street number like 838 or 1004.
+ * 12528 – ENVIRONMENT PROTECTION
+ * http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=3973
  * 
- * The Queen of Nlogonia ordered a new seaside avenue to be built, and wants to assign to 
- * the new houses only numbers without repeated digits, to avoid discomfort among her subjects. 
- * You have been appointed by Her Majesty to write a program that, given two integers N and M, 
- * determines the maximum number of houses that can be assigned street numbers between N and M, 
- * inclusive, that do not have repeated digits.
+ * Arsenic & Cyanide Mining (ACM) is a corporation that has recently decided to start 
+ * developing its mines in the lands near your hometown. As a member of the citizen's 
+ * regulatory committee for ACM's operations, your task is to control how much the 
+ * corporation can mine from those lands, so that you get to keep the jobs and other 
+ * benefits without sacrificing the environment and the health of the local residents...
  */
-package differentdigits;
+package environmentprotection;
 
 import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.text.DecimalFormat;
 import java.util.StringTokenizer;
 
 public class Naive {
 
+	public static double W, D, A, K;
+	public static double[] p1 = new double[10];
+	public static double[] q1 = new double[10];
+	public static double[] p2 = new double[10];
+	public static double[] q2 = new double[10];
+
+	public static double eval(double x, double line) {
+		double num1, den1, num2, den2;
+		num1 = num2 = den1 = den2 = 0.0;
+		for (int i = (int) K; i >= 0; i--) {
+			num1 += Math.pow(x, i) * p1[i];
+			den1 += Math.pow(x, i) * q1[i];
+			num2 += Math.pow(x, i) * p2[i];
+			den2 += Math.pow(x, i) * q2[i];
+		}
+
+		double integral1, integral2;
+		integral1 = num1 / den1;
+		integral2 = num2 / den2;
+
+		if (line > integral1) {
+			return 0;
+		} else if (line < integral2) {
+			return integral1 - integral2;
+		} else
+			return integral1 - line;
+	}
+	
+	public static double simps(double a, double b, double line) {
+		return ((b - a) / 6.0)
+				* (eval(a, line) + 
+					(4 * eval((a + b) / 2.0, line)) + 
+					eval(b, line));
+	}
+
 	public static void main(String[] args) {
 		try {
-			InputStream input = new DataInputStream(new FileInputStream(
-			"src/differentdigits/digits.in"));
-			//InputStream input = System.in;
+			//InputStream input = new DataInputStream(new FileInputStream(
+				//	"src/environmentprotection/env.in"));
+			InputStream input = System.in;
 
 			BufferedReader reader = new BufferedReader(new InputStreamReader(
 					input));
@@ -38,40 +68,67 @@ public class Naive {
 			while ((params = reader.readLine()) != null) {
 				tokenizer = new StringTokenizer(params);
 
-				int n = Integer.parseInt(tokenizer.nextToken());
-				int m = Integer.parseInt(tokenizer.nextToken());
-				
-				int c = 0;
+				W = Integer.parseInt(tokenizer.nextToken());
+				D = Integer.parseInt(tokenizer.nextToken());
+				A = Integer.parseInt(tokenizer.nextToken());
+				K = Integer.parseInt(tokenizer.nextToken());
 
-				for (int i=n; i<=m; i++) {
-					c += different(i);
+				params = reader.readLine();
+				tokenizer = new StringTokenizer(params);
+
+				for (int i = 0; i <= K; i++) {
+					p1[i] = Double.parseDouble(tokenizer.nextToken());
 				}
-				
-				System.out.println(c);
+
+				params = reader.readLine();
+				tokenizer = new StringTokenizer(params);
+
+				for (int i = 0; i <= K; i++) {
+					q1[i] = Double.parseDouble(tokenizer.nextToken());
+				}
+
+				params = reader.readLine();
+				tokenizer = new StringTokenizer(params);
+
+				for (int i = 0; i <= K; i++) {
+					p2[i] = Double.parseDouble(tokenizer.nextToken());
+				}
+
+				params = reader.readLine();
+				tokenizer = new StringTokenizer(params);
+
+				for (int i = 0; i <= K; i++) {
+					q2[i] = Double.parseDouble(tokenizer.nextToken());
+				}
+
+				double eps = 1e-4;
+				double x;
+				double low = -D;
+				double high = 0;
+
+				for (int i = 0; i < 22; i++) {
+					double totalArea = 0.0;
+					x = (low + high) / 2.0;
+
+					for (double a = 0; a + eps - 1e-5 < W; a += eps) {
+						double b = a + eps;
+						totalArea += simps(a, b, x);
+					}
+
+					if (totalArea < A) {
+						high = x;
+					} else {
+						low = x;
+					}
+				}
+
+				DecimalFormat numberFormat = new DecimalFormat("#.00000");
+				System.out.println(numberFormat.format(-low));
 			}
 
 			reader.close();
-
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-
-	public static int different(int arg) {
-		int[] dig = new int[12];
-
-		for (int i = 0; i < 12; i++) {
-			dig[i] = 0;
-		}
-
-		while (arg > 0) {
-			dig[arg % 10]++;
-			if (dig[arg % 10] > 1)
-				return 0;
-			arg /= 10;
-		}
-
-		return 1;
-	}
-
 }
